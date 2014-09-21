92ce983145de955c0ec611d3bbbbd23b1bd93745
Jian Fang <jian.fang.gm@gmail.com>
2014-09-21 22:23:10 +0800

    Data model for apps

---
+++ src/data_model.py
@@ -0,0 +1,29 @@
+__author__ = 'sid'
+
+class AppUpdate:
+    version = "1.0"
+    updated = "01-01-2007"
+    price = "Free"
+    size = "1kb"
+    whatsnew = "NA"
+
+
+class App:
+    # basic
+    name = "app name"
+    category = "NA"
+    description = "NA"
+    seller = "NA"
+
+    # extended
+    rated = "NA"
+    ratings_current = 0
+    ratings_all = 0
+
+    image = "NA"
+    screenshots_iphone = []
+    screenshots_ipad = []
+
+
+
+
